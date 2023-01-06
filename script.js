6ed7ee7001678f0609f5cd7da6cb89709d3cd3bb
Kawoschu <45479033+Kawoschu@users.noreply.github.com>
2023-01-06 11:36:30 -0300

    Project uploaded

---
+++ script.js
@@ -0,0 +1,33 @@
+const dia = document.getElementById('dia')
+const hora = document.getElementById('hora')
+const minuto = document.getElementById('minuto')
+const segundo = document.getElementById('segundo')
+const lancamento = "23 JAN 2023"
+function countdown(){
+const datalanc = new Date(lancamento)
+const hoje = new Date()
+
+const segTotal = (datalanc - hoje)/1000
+
+const finalDias = Math.floor(segTotal / 60 / 60 / 24);
+const finalHoras = Math.floor(segTotal / 60 / 60) % 24;
+const finalMinutos = Math.floor(segTotal / 60) % 60;
+const finalSegundos = Math.floor(segTotal) % 60;
+
+dia.innerHTML = finalDias +`D`  
+hora.innerHTML = formatoTempo(finalHoras)+`H`  
+minuto.innerHTML = formatoTempo(finalMinutos)+`M`  
+segundo.innerHTML = formatoTempo(finalSegundos)+`S`  
+
+}
+function formatoTempo(tempo) {
+    return tempo < 10 ? `0${tempo}` : tempo;
+  }
+countdown();
+setInterval(countdown,1000)
+
+
+function highlight(selector){
+var element = document.querySelector(selector);
+element.classList.toggle("card-highlight")
+}
